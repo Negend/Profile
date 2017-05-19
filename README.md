948c75b99213293b93c881c98e3b97cc01147c47
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-05-19 01:05:10 +0100

    perfected all functions including after a win

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 #Tic Tac Toe
 
-made a game
+##work process
+Literally my whole day but my baby is ready and she works 
+
+I was got by the simple things like not knowing to use
+
+ ```
+ this.value = X instead of buttons[i] = X
+ 
+ ```
+ 
+ also as I had little tim towards the end my coding is long and messy for calculating the winner. definitely need to brainstorm a smarter and neater approach.
+ 
+##summary
+* works well can be used. 
+* stops and announces the winner. 
+* Does not allow overwiting of buttons
+* gives option to restart with astart button 
--- main.js
+++ main.js
@@ -1,27 +1,6 @@
 //winning combos
 
 
-// var one = score[0]
-// var two = score[1]
-// var three = score[2]
-// var four = score[3]
-// var five = score[4]
-// var six = score[5]
-// var seven = score[6]
-// var eight = score[7]
-// var nine = score[8]
-
-
-
-// var a = one + two + three 
-// var b = one + four +seven
-// var c = four + five + six
-// var d = seven + eight +nine
-// var e = one + five + nine
-// var f = three +five + seven
-// var g = two +five +eight 
-// var h = three +six +nine
-
 
 var buttons = document.getElementsByClassName('turn')
 var start = document.getElementsByClassName('start')
@@ -78,22 +57,24 @@ for(var i = 0; i < 9; i++){
 	
 
 	button.addEventListener('click',function(event){
-		if(this.value != 'O' && this.value != 'X'){
-			counter.push('1')
-			
-			var n = counter.length		
-			if(n % 2 == 0){
+		if( print[0].innerHTML == 'Tic Tac Hoe'){	
+			if(this.value != 'O' && this.value != 'X'){
+				counter.push('1')
 				
-				this.value = ('X')
-				score[n-1] =('X')
-				won()
-			}else{
-				this.value = ('O')
-				score[n-1] = ('O')
-				won()
-				
-			}	
-		} 
+				var n = counter.length		
+				if(n % 2 == 0){
+					
+					this.value = ('X')
+					score[n-1] =('X')
+					won()
+				}else{
+					this.value = ('O')
+					score[n-1] = ('O')
+					won()
+					
+				}	
+			} 
+		}	
 	})
 
 }
